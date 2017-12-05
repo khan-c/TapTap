febcf7fe0e2a9b01d4fc642bfda6b78e78ca21b6
Kyle Chen <kyle.h.chen@gmail.com>
2017-12-05 10:24:15 -0800

    Work on styling of game

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ import GameView from './lib/game_view';
 
 document.addEventListener("DOMContentLoaded", e => {
   const canvas = document.getElementById("myCanvas");
-  canvas.style.backgroundColor = "rgba(77, 147, 240, 0.25)";
+  canvas.style.background = "radial-gradient(rgba(5, 56, 6, 0.4), rgba(30, 157, 8, 0.7))";
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
   const ctx = canvas.getContext('2d');
 
   const gameView = new GameView(ctx, canvas);
-  gameView.load();
 });
