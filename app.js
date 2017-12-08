39dfa27cddd75cb0584b13f8feb17d1161e530a8
Kyle Chen <kyle.h.chen@gmail.com>
2017-12-08 11:53:06 -0800

    Test debug approach for node heroku postgres issue

--- app.js
+++ app.js
@@ -22,39 +22,40 @@ app.use(express.static('frontend'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(process.env.PORT || PORT, () => {
+  console.log(__dirname);
+  console.log(`listening on ${PORT}`);
+});
+
+// const client = new pg.Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true
+// });
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './frontend/index.html'));
 });
 
-pool.connect((err, client, done) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(client);
-  app.listen(process.env.PORT || PORT, () => {
-    console.log(__dirname);
-    console.log(`listening on ${PORT}`);
-  });
+// client.connect();
 
-  myClient = client;
-
-
-  app.get('/scores', (req, res) => {
+app.get('/scores', (req, res) => {
+  return pool.connect().then(theclient => {
     const scoresQuery = format('SELECT * FROM scores ORDER BY score DESC LIMIT 15');
-    myClient.query(scoresQuery, (errors, results) => {
+    theclient.query(scoresQuery, (errors, results) => {
       if (errors) {
         console.log(errors);
       }
       res.send(results.rows);
     });
   });
+});
 
-
-  app.post('/scores', (req, res) => {
+app.post('/scores', (req, res) => {
+  return pool.connect.then(theClient => {
     const { name, score } = req.body;
     const data = [name, score];
     const postQuery = format("INSERT INTO scores VALUES (%L)", data);
-    myClient.query(postQuery, (errors, results) => {
+    theClient.query(postQuery, (errors, results) => {
       if (errors) {
         console.log(errors);
       }
@@ -62,3 +63,15 @@ pool.connect((err, client, done) => {
     });
   });
 });
+
+// pool.connect((err, client, done) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//
+//   myClient = client;
+//
+//
+//
+//
+// });
